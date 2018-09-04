02f1db68cab8c53c2baee09744874b3d9bf9b8da
ChillerDragon <chillerdragon@gmail.com>
2018-09-04 22:12:30 +0200

    Delete users with sql query

---
+++ lib/query-delete.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "=============================="
+echo "delete users with sql query!"
+echo "warning powerfull script!"
+echo "=============================="
+echo "Create an sql query returning usernames."
+echo "All those users and their configs will be deleted!"
+echo ""
+echo "examples:"
+echo "SELECT Username FROM Accounts WHERE RegisterDate >= Datetime('2009-11-13 00:00:00');"
+read -p "SQL> " query
+aUsers=(`echo "$query" | sqlite3 ../users.db`)
+echo "Selected users:"
+for user in "${aUsers[@]}"
+do
+	echo $user
+done
+echo "---------------------"
+echo "do you really want to delete these users? [y/n]"
+read -n 1 -p "" inp
+echo ""
+if [ "$inp" == "n" ]; then
+    printf "\n[TestVPN] stopped.\n"
+    exit
+elif [ "$inp" == "N" ]; then
+    printf "\n[TestVPN] stopped.\n"
+    exit
+fi
+
+for user in "${aUsers[@]}"
+do
+	./deluser-web.sh $user
+done
